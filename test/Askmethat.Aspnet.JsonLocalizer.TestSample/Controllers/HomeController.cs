83839955eae2d10404334c06c86532c745ed2db1
Alexandre Teixeira <askmethatfr@gmail.com>
2017-12-07 21:03:05 +0100

    Adding some changes to have it working in macos

--- test/Askmethat.Aspnet.JsonLocalizer.TestSample/Controllers/HomeController.cs
+++ test/Askmethat.Aspnet.JsonLocalizer.TestSample/Controllers/HomeController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 
 namespace Askmethat.Aspnet.JsonLocalizer.TestSample.Controllers
 {
     public class HomeController : Controller
     {
+        IStringLocalizer _localizer;
+        public HomeController(IStringLocalizerFactory factory)
+        {
+            _localizer = factory.Create(typeof(IStringLocalizer));
+        }
         public IActionResult Index()
         {
             return View();
