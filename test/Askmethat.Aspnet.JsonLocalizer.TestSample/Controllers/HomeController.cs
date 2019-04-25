201ef445232ac1417cea1d70b4cadb2723aa87c9
Alexandre Teixeira <alexandre.t@yoopala.com>
2019-04-25 12:51:31 +0200

    working cookie template #48

--- test/Askmethat.Aspnet.JsonLocalizer.TestSample/Controllers/HomeController.cs
+++ test/Askmethat.Aspnet.JsonLocalizer.TestSample/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
 
@@ -13,5 +15,18 @@ namespace Askmethat.Aspnet.JsonLocalizer.TestSample.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect(returnUrl);
+        }
+
     }
 }
