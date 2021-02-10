c8973f4b5b5b0621be1df683d094eb736a1745a3
Alexandre Teixeira <askmethatfr@gmail.com>
2021-02-10 17:15:52 +0100

    fix interface

--- Askmethat.Aspnet.JsonLocalizer/Localizer/IJsonStringLocalizer.cs
+++ Askmethat.Aspnet.JsonLocalizer/Localizer/IJsonStringLocalizer.cs
@@ -10,6 +10,7 @@ namespace Askmethat.Aspnet.JsonLocalizer.Localizer
 	    void ClearMemCache(IEnumerable<CultureInfo> culturesToClearFromCache = null);
 	    void ReloadMemCache(IEnumerable<CultureInfo> culturesToClearFromCache = null);
 	    new IStringLocalizer WithCulture(CultureInfo culture);
+	    LocalizedString GetPlural(string key, double count, params object[] arguments);
 	    MarkupString GetHtmlBlazorString(string name, bool shouldTryDefaultCulture = true);
     }
 
